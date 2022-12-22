2bfa28927b13bb2b9c498c7a2d095df44aa758d3
César Olvera <olvera_cesar.m@hotmail.com>
2022-12-21 19:55:55 -0600

    datos basicos de js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let button = document.querySelector('button');
+button.onclick =function () {
+    let cro = 550;
+    let rop = 125;
+    let col = 125;
+    let cor = 110;
+    let sha = 475;
+    alert('Escriba "cli" si es Cliente o "due" si es Dueño ')
+    let usu = prompt('¿Quien eres?')
+    if (usu == "due") {
+
+    }else if (usu == "cli"){
+
+    }
+}
